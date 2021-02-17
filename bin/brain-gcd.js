b416828b3ce382980f5cc4a68f0d6d2f0f09c07a
Yurii Polishchuk <yr.polishchuk@gmail.com>
2021-02-17 11:53:26 +0200

    add brain-gcd game

---
+++ bin/brain-gcd.js
@@ -0,0 +1,7 @@
+#!/usr/bin/env node
+
+/* eslint-disable  import/extensions */
+import startGame from '../src/index.js';
+import gcdGame from '../src/games/gcd-game.js';
+
+startGame(gcdGame);
---
+++ src/games/gcd-game.js
@@ -0,0 +1,34 @@
+const stringToNumbersArr = (str) => {
+  const numbers = [];
+  let currentNum = '';
+  for (let i = 0; i <= str.length; i += 1) {
+    if (str[i] === ' ' || i === str.length) {
+      numbers.push(Number(currentNum));
+      currentNum = '';
+    } else {
+      currentNum += str[i];
+    }
+  }
+  return numbers;
+};
+
+const gcd = (arr) => {
+  const [a, b] = arr.sort((x, y) => y - x);
+  if (a % b === 0) {
+    return b;
+  }
+  return gcd([(a % b), b]);
+};
+
+const gcdGame = () => {
+  const condition = 'Find the greatest common divisor of given numbers.';
+  const expression = () => {
+    const num1 = Math.floor(Math.random() * 100);
+    const num2 = Math.floor(Math.random() * 100);
+    return `${num1} ${num2}`;
+  };
+  const checkResult = (str) => gcd(stringToNumbersArr(str));
+  return [condition, expression, checkResult];
+};
+
+export default gcdGame;
