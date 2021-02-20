ca9208d93bfec0b7edeb10fc5437f6e25518ef5f
Yurii Polishchuk <yr.polishchuk@gmail.com>
2021-02-20 10:46:12 +0200

    change index file

--- src/index.js
+++ src/index.js
@@ -4,21 +4,18 @@ import readlineSync from 'readline-sync';
 import greeting from './cli.js';
 
 const startGame = (gameName) => {
-  const [condition, expression, checkResult] = gameName();
+  const [condition, expression, check] = gameName();
   const name = greeting();
 
   console.log(condition);
 
-  let customerAnswer = null;
-  let correctAnswer = null;
-
   for (let i = 0; i < 3; i += 1) {
     const currentQuestion = expression();
     console.log(`Question: ${currentQuestion}`);
-    correctAnswer = checkResult(currentQuestion);
-    customerAnswer = Number(readlineSync.question('Your answer is: '));
+    const customerAnswer = readlineSync.question('Your answer is: ');
+    const [correctAnswer, checkResult] = check(currentQuestion, customerAnswer);
 
-    if (correctAnswer !== customerAnswer) {
+    if (!checkResult) {
       console.log(`'${customerAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'`);
       console.log(`Let's try again, ${name}!`);
       return false;
