285a2105b9c266343a3392aca094b7e592955a9f
Yurii Polishchuk <yr.polishchuk@gmail.com>
2021-02-20 11:01:34 +0200

    fix brain-even game

--- src/games/even-game.js
+++ src/games/even-game.js
@@ -1,23 +1,17 @@
 /* eslint-disable no-console */
-
-const isEven = (num) => {
-  if (num % 2 === 0) {
-    return true;
-  }
-  return false;
-};
-
 const evenGame = () => {
   const condition = 'Answer "yes" if the number is even, otherwise answer "no".';
+
   const expression = () => Math.floor(Math.random() * 100);
-  const checkResult = (numb) => {
-    if (isEven(numb)) {
-      return 'yes';
-    }
-    return 'no';
+
+  const check = (expr, answer) => {
+    const isEven = (num) => (num % 2 === 0);
+    const correctAnswer = (isEven(expr)) ? 'yes' : 'no';
+    const checkResult = correctAnswer === answer;
+    return [correctAnswer, checkResult];
   };
 
-  return [condition, expression, checkResult];
+  return [condition, expression, check];
 };
 
 export default evenGame;
