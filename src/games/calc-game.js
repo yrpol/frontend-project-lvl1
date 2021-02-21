161b1b91d76da3495be69002628943906ef0e7f2
yrpol <yrpol87@gmail.com>
2021-02-21 12:53:40 +0200

    Update calc-game.js

--- src/games/calc-game.js
+++ src/games/calc-game.js
@@ -2,22 +2,24 @@
 import _ from 'lodash';
 import { evaluate } from 'mathjs';
 
-const condition = 'What is the result of the expression?';
-
-const expression = () => {
+const calcGame = () => {
+  const condition = 'What is the result of the expression?';
+  
+  const expression = () => {
   const operands = ['+', '-', '*'];
   const num1 = Math.floor(Math.random() * 100);
   const num2 = Math.floor(Math.random() * 100);
   const operand = _.sample(operands);
   return `${num1} ${operand} ${num2}`;
 };
-
-const check = (exrp, answer) => {
+  
+  const check = (exrp, answer) => {
   const correctAnswer = evaluate(exrp);
   const checkResult = correctAnswer === Number(answer);
   return [correctAnswer, checkResult];
 };
-
-const calcGame = () => [condition, expression, check];
+  
+  return [condition, expression, check];
+}
 
 export default calcGame;
