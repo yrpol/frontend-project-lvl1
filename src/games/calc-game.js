b91d7b43c11a38d669c50c4fe2a5293de6f0fbb0
Yurii Polishchuk <yr.polishchuk@gmail.com>
2021-02-21 11:51:39 +0200

    refactored calc-game

--- src/games/calc-game.js
+++ src/games/calc-game.js
@@ -2,22 +2,22 @@
 import _ from 'lodash';
 import { evaluate } from 'mathjs';
 
-const calcGame = () => {
-  const condition = 'What is the result of the expression?';
-  const expression = () => {
-    const operands = ['+', '-', '*'];
-    const num1 = Math.floor(Math.random() * 100);
-    const num2 = Math.floor(Math.random() * 100);
-    const operand = _.sample(operands);
-    return `${num1} ${operand} ${num2}`;
-  };
-  const check = (exrp, answer) => {
-    const correctAnswer = evaluate(exrp);
-    const checkResult = correctAnswer === Number(answer);
-    return [correctAnswer, checkResult];
-  };
+const condition = 'What is the result of the expression?';
 
-  return [condition, expression, check];
+const expression = () => {
+  const operands = ['+', '-', '*'];
+  const num1 = Math.floor(Math.random() * 100);
+  const num2 = Math.floor(Math.random() * 100);
+  const operand = _.sample(operands);
+  return `${num1} ${operand} ${num2}`;
 };
+
+const check = (exrp, answer) => {
+  const correctAnswer = evaluate(exrp);
+  const checkResult = correctAnswer === Number(answer);
+  return [correctAnswer, checkResult];
+};
+
+const calcGame = () => [condition, expression, check];
 
 export default calcGame;
