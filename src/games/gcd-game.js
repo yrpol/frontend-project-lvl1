9680c2576ff5312e7d66a006a814dfee525eecb8
Yurii Polishchuk <yr.polishchuk@gmail.com>
2021-02-21 11:54:04 +0200

    refactored gcd-game

--- src/games/gcd-game.js
+++ src/games/gcd-game.js
@@ -1,3 +1,11 @@
+const condition = 'Find the greatest common divisor of given numbers.';
+
+const expression = () => {
+  const num1 = Math.floor(Math.random() * (100 - 1) + 1);
+  const num2 = Math.floor(Math.random() * (100 - 1) + 1);
+  return `${num1} ${num2}`;
+};
+
 const gcd = (arr) => {
   const [a, b] = arr.sort((x, y) => y - x);
   if (a % b === 0) {
@@ -6,23 +14,13 @@ const gcd = (arr) => {
   return gcd([(a % b), b]);
 };
 
-const gcdGame = () => {
-  const condition = 'Find the greatest common divisor of given numbers.';
-
-  const expression = () => {
-    const num1 = Math.floor(Math.random() * (100 - 1) + 1);
-    const num2 = Math.floor(Math.random() * (100 - 1) + 1);
-    return `${num1} ${num2}`;
-  };
-
-  const check = (expr, answer) => {
-    const numbers = expr.split(' ').map(Number);
-    const correctAnswer = gcd(numbers);
-    const checkResult = correctAnswer === Number(answer);
-    return [correctAnswer, checkResult];
-  };
-
-  return [condition, expression, check];
+const check = (expr, answer) => {
+  const numbers = expr.split(' ').map(Number);
+  const correctAnswer = gcd(numbers);
+  const checkResult = correctAnswer === Number(answer);
+  return [correctAnswer, checkResult];
 };
+
+const gcdGame = () => [condition, expression, check];
 
 export default gcdGame;
