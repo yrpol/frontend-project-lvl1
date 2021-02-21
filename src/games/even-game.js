a1dce6442c63041b5d55bb1846e616a433fbe012
Yurii Polishchuk <yr.polishchuk@gmail.com>
2021-02-21 11:52:51 +0200

    refactored even-game

--- src/games/even-game.js
+++ src/games/even-game.js
@@ -1,17 +1,15 @@
 /* eslint-disable no-console */
-const evenGame = () => {
-  const condition = 'Answer "yes" if the number is even, otherwise answer "no".';
+const condition = 'Answer "yes" if the number is even, otherwise answer "no".';
 
-  const expression = () => Math.floor(Math.random() * 100);
+const expression = () => Math.floor(Math.random() * 100);
 
-  const check = (expr, answer) => {
-    const isEven = (num) => (num % 2 === 0);
-    const correctAnswer = (isEven(expr)) ? 'yes' : 'no';
-    const checkResult = correctAnswer === answer;
-    return [correctAnswer, checkResult];
-  };
-
-  return [condition, expression, check];
+const check = (expr, answer) => {
+  const isEven = (num) => (num % 2 === 0);
+  const correctAnswer = (isEven(expr)) ? 'yes' : 'no';
+  const checkResult = correctAnswer === answer;
+  return [correctAnswer, checkResult];
 };
+
+const evenGame = () => [condition, expression, check];
 
 export default evenGame;
