5d73773645256f9dd67c83302f604ebc5dcebf9d
Yurii Polishchuk <yr.polishchuk@gmail.com>
2021-02-21 11:50:05 +0200

    refactored prime-game

--- src/games/prime-game.js
+++ src/games/prime-game.js
@@ -1,25 +1,24 @@
-const primeGame = () => {
-  const condition = 'Answer "yes" if given number is prime. Otherwise answer "no".';
+const condition = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 
-  const expression = () => Math.floor(Math.random() * 100);
+const expression = () => Math.floor(Math.random() * 100);
 
-  const check = (expr, answer) => {
-    const isPrime = (num) => {
-      if (num < 1) {
+const check = (expr, answer) => {
+  const isPrime = (num) => {
+    if (num < 1) {
+      return false;
+    }
+    for (let i = 2; i < num; i += 1) {
+      if (num % i === 0) {
         return false;
       }
-      for (let i = 2; i < num; i += 1) {
-        if (num % i === 0) {
-          return false;
-        }
-      }
-      return true;
-    };
-    const correctAnswer = isPrime(expr) ? 'yes' : 'no';
-    const checkResult = correctAnswer === answer;
-    return [correctAnswer, checkResult];
+    }
+    return true;
   };
-  return [condition, expression, check];
+  const correctAnswer = isPrime(expr) ? 'yes' : 'no';
+  const checkResult = correctAnswer === answer;
+  return [correctAnswer, checkResult];
 };
+
+const primeGame = () => [condition, expression, check];
 
 export default primeGame;
