9bcb9254b14064049d8014d3f9639173ff7c5b30
Yurii Polishchuk <yr.polishchuk@gmail.com>
2021-02-22 13:12:00 +0200

    refactoring calc-game without mathjs

--- src/games/calc-game.js
+++ src/games/calc-game.js
@@ -1,9 +1,7 @@
 /* eslint-disable no-console */
 import _ from 'lodash';
-import { evaluate } from 'mathjs';
 
 const condition = 'What is the result of the expression?';
-
 const expression = () => {
   const operands = ['+', '-', '*'];
   const num1 = Math.floor(Math.random() * 100);
@@ -11,8 +9,25 @@ const expression = () => {
   const operand = _.sample(operands);
   return `${num1} ${operand} ${num2}`;
 };
-
 const check = (exrp, answer) => {
+  const evaluate = (str) => {
+    const symbols = str.split(' ');
+    const [first, sign, last] = symbols;
+
+    const recognizer = (operator, x, y) => {
+      if (operator === '+') {
+        return x + y;
+      }
+      if (operator === '-') {
+        return x - y;
+      }
+      if (operator === '*') {
+        return x * y;
+      }
+      return null;
+    };
+    return recognizer(sign, Number(first), Number(last));
+  };
   const correctAnswer = evaluate(exrp);
   const checkResult = correctAnswer === Number(answer);
   return [correctAnswer, checkResult];
